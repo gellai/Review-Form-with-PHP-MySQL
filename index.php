9dd97b601c3022d2fbac1021439fb55415d83f91
gellai <>
2020-01-31 14:21:02 +0000

    Typo fix

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ $param = array(
             <?php endif; ?>
 
             <h1>Customer Reviews</h1>
-            <? if(isset($reviews)): ?>
+            <?php if(isset($reviews)): ?>
                 <ul class="list-group">
                     <?php foreach($reviews as $review): ?>
                         <form><li class="list-group-item">
